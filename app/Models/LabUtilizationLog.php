d46436beb51bed022c7980c56ac62fa710186f1b
Jemp72 <pedrosoemman72@gmail.com>
2026-05-21 19:21:34 +0800

    Create Lab utilization log model

--- app/Models/LabUtilizationLog.php
+++ app/Models/LabUtilizationLog.php
@@ -2,13 +2,53 @@
 
 namespace App\Models;
 
-class labUtilizationLog
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class LabUtilizationLog extends Model
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    use HasFactory;
+
+    protected $table = 'lab_utilization_logs';
+
+    protected $primaryKey = 'lab_utilization_log_id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'student_id',
+        'guest_log_id',
+        'purpose',
+        'instructor_id',
+        'lab_id',
+        'log_date',
+        'time_in',
+        'time_out',
+    ];
+
+    protected $casts = [
+        'log_date' => 'date',
+        'time_in' => 'datetime',
+        'time_out' => 'datetime',
+    ];
+
+    public function student()
     {
-        //
+        return $this->belongsTo(Student::class, 'student_id');
+    }
+
+    public function guest()
+    {
+        return $this->belongsTo(GuestLog::class, 'guest_log_id');
+    }
+
+    public function instructor()
+    {
+        return $this->belongsTo(SystemUser::class, 'instructor_id');
+    }
+
+    public function laboratory()
+    {
+        return $this->belongsTo(Laboratory::class, 'lab_id');
     }
 }
