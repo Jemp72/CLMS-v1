607e397a8463723bc7b7be2416d2e5eea31bb3d5
Xeno-nix <jnclarido03202400026@usep.edu.ph>
2026-05-23 12:35:15 +0800

    feat: route for booking

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('bookings')->name('bookings.')->group(function () {
+    Route::get('/',                        [BookingController::class, 'index'])->name('index');
+    Route::get('/lab/{labId}',             [BookingController::class, 'byLab'])->name('byLab');
+    Route::post('/',                       [BookingController::class, 'store'])->name('store');
+    Route::get('/{id}',                    [BookingController::class, 'show'])->name('show');
+    Route::patch('/{id}/status',           [BookingController::class, 'updateStatus'])->name('updateStatus');
+    Route::delete('/{id}',                 [BookingController::class, 'destroy'])->name('destroy');
 });
